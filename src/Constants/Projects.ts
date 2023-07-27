ff7259fa807d8b86fd755077879467c9bc778e1b
Sulaimon Adeoye <adeoyesulaimon@yahoo.com>
2023-07-27 15:04:32 +0100

    made some changes

--- src/Constants/Projects.ts
+++ src/Constants/Projects.ts
@@ -1,45 +1,30 @@
 import  DesignSolution  from "../starter-code/assets/images/thumbnail-project-1-large.webp"
 import  Skilled2  from "../starter-code/assets/images/thumbnail-project-2-large.webp"
-
+import MoviePic from "../starter-code/assets/images/movieimage.jpeg"
+import MarsPic from "../starter-code/assets/images/marsrover.jpeg"
+import EcommercePic from "../starter-code/assets/images/shoppingimages.jpeg"
 
 export const Projects = [
     {
         id : 1,
-        projectImage : DesignSolution,
-        title: "SKILLED", 
-        toolsUsed:"REACT    HTML  "
+        projectImage : MoviePic,
+        title: "MOVIE", 
+        toolsUsed:"REACT HTML CSS JAVASCRIPT"
 
         
     },
     {
         id : 2,
-        projectImage : Skilled2,
-        title: "SKILLED", 
-        toolsUsed:"REACT    HTML  TYPESCRIPT "
+        projectImage :  MarsPic,
+        title: "MARS ROVER", 
+        toolsUsed:"REACT  NASA API HTML CSS"
     },
     {
         id : 3,
-        projectImage : Skilled2,
-        title: "SKILLED", 
-        toolsUsed:"REACT    HTML  TYPESCRIPT "
+        projectImage : EcommercePic,
+        title: "ECOMMERCE PLATFORM", 
+        toolsUsed:"REACT    HTML  CSS FIREBASE"
     },
-    {
-        id : 4,
-        projectImage : Skilled2,
-        title: "SKILLED", 
-        toolsUsed:"REACT    HTML  TYPESCRIPT "
-    },
-    {
-        id : 5,
-        projectImage : Skilled2,
-        title: "SKILLED", 
-        toolsUsed:"REACT    HTML  TYPESCRIPT "
-    },
-    {
-        id : 6,
-        projectImage : Skilled2,
-        title: "SKILLED", 
-        toolsUsed:"REACT    HTML  TYPESCRIPT "
-    },
+   
 ]
 
--- src/Layout/Hero/index.tsx
+++ src/Layout/Hero/index.tsx
@@ -2,7 +2,7 @@ import React, { ReactElement } from 'react'
 
 import { Link } from "react-scroll";
 
-import profilePic from "../../starter-code/assets/images/sulaimon.jpg"
+import profilePic from "../../starter-code/assets/images/image-profile-desktop.webp"
 import github from "../../starter-code/assets/images/icon-github.svg"
 import linkedin from "../../starter-code/assets/images/icon-linkedin.svg"
 import twitter from   "../../starter-code/assets/images/icon-twitter.svg"
@@ -14,19 +14,19 @@ interface Props {
 
 function Hero({}: Props): ReactElement {
     return (
-        <div className='h-screen bg-black  relative'>
+        <div className='md:h-screen bg-black  relative'>
              <img className = "absolute top-[23%]" src = {Oval} alt = "oval"/>
             <div className='md:w-[80%] md:h-[100%] border-b-2 border-white md:grid md:grid-cols-2 md:gap-4 mx-auto '>
               <div className='md:text-left my-[100px]'>
-                 <h1 className='md:text-[60px] text-white'>Nice to meet you! </h1>
+                 <h1 className='text-[20px] md:text-[60px] text-white'>Nice to meet you! </h1>
                  <div className='mt-4 flex-col md:flex md:flex-row'>
-                   <h1 className='md:text-[60px] text-white '>I'm  </h1> <h1 className='border-b-2 border-[#4EE1A0] md:text-[60px] text-white  md:border-[#4EE1A0] md:border-b-2 pb-0 md:ml-2'>Adeoye Sulaimon.</h1>
+                   <h1 className='md:text-[60px] text-white '>I'm  </h1> <h1 className=' md:w-100 border-b-2 border-[#4EE1A0] text-white md:text-[60px] md:border-[#4EE1A0] md:border-b-2 pb-0 md:ml-2' >Adeoye Sulaimon.</h1>
                  </div>
                 
                  <p className='text-sm text-white'>
                     Based in the Lagos, I’m a front-end developer <br/> passionate about building accessible web apps <br/> that users love.
                  </p>
-                 <div className='md:text-left mt-[10%]'>
+                 <div className='md:text-left mt-[10%] gap-4'>
                   <Link to= "contacts" spy={true} smooth={true} offset={-10} duration={500}>
                     <button 
                     className='text-white border-b-2 border-[#4EE1A0] md:pb-4 hover:opacity-50 hover:text-[#4EE1A0]'
@@ -34,15 +34,20 @@ function Hero({}: Props): ReactElement {
                     >Contact Me</button>
                   
                   </Link>
-                    
-                    
+                  
+                  <a className = "ml-4" href = "../../starter-code/assets/images/resume2.pdf" download = "/starter-code/assets/images/resume2.pdf">
+                    <button 
+                     className='text-white border-b-2 border-[#4EE1A0] md:pb-4 hover:opacity-50 hover:text-[#4EE1A0]'
+                    >
+                      Resume
+                    </button>
+                    </a> 
                  </div>
                </div>
                <div 
-               className = "z-30 mx-auto h-[300px] md:mt-[-100px] md:h-[90%] md:w-full bg-center bg-cover relative" 
-               style = {{ backgroundImage : `url(${profilePic})` }}
+                 className = "z-30 mx-auto h-[300px] md:mt-[-100px] md:h-[90%] md:w-full bg-center bg-cover relative" 
+                 style = {{ backgroundImage : `url(${profilePic})` }}
                >
-                 
                    <div className='grid grid-cols-4 md:w-[30%] gap-4 right-2 absolute md:top-[5%]'>
                      <a href="https://www.github.com/adeoye99">
                        <img  src= {github}  alt = "githublogo"/>
@@ -51,12 +56,11 @@ function Hero({}: Props): ReactElement {
                         <img  src= {linkedin}  alt = "linkedinlogo"/>
                      </a>
                       <a>
-                        <img  src= {frontendMentor}  alt = "linkedinlogo"/>
+                        <img  src= {frontendMentor}  alt = "linkedinlogo" />
                       </a>
-                     <a href="https://twitter.com/SulaimonAdeoye5" >
-                       <img  src= {twitter}  alt = "twitterlogo"/>
+                     <a href = "https://twitter.com/SulaimonAdeoye5" >
+                       <img  src  = {twitter}  alt = "twitterlogo"/>
                      </a>
-                     
                   </div> 
                </div>
             </div>
