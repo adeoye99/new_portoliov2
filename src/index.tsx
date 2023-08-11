de13812cf37ae9bc24971aac9a9f095c04e33c14
Sulaimon Adeoye <adeoyesulaimon@yahoo.com>
2023-08-11 16:27:01 +0100

    made some changes

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tailwindcss/tailwind.css';
+import firebase from 'firebase/app';
 
+// ...
+
+// const firebaseConfig = {
+//   apiKey: 'YOUR_API_KEY',
+//   authDomain: 'YOUR_AUTH_DOMAIN',
+//   projectId: 'YOUR_PROJECT_ID',
+//   storageBucket: 'YOUR_STORAGE_BUCKET',
+//   messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+//   appId: 'YOUR_APP_ID',
+// };
+
+// firebase.initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
@@ -16,5 +30,5 @@ root.render(
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vital
 reportWebVitals();
